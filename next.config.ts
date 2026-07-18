5bb76a678e6d26dad8037fb70542e903c0cd6a5c
golamrabbi73 <golamrabbi39hr@gmail.com>
2026-07-18 10:35:50 +0600

    fix(oauth): add referrer-policy and trim env variables

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,10 @@ const nextConfig: NextConfig = {
             key: 'Cross-Origin-Opener-Policy',
             value: 'same-origin-allow-popups',
           },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
         ],
       },
     ];
